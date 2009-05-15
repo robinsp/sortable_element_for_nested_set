00384932f197f99c8cbad06ace814d959633e3ba
Robin Spainhour <robin@robinspainhour.com>
2009-05-15 15:12:08 +0200

    Removed comments

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,7 @@
-#begin
-#  require File.dirname(__FILE__) + '/../../../../spec/spec_helper'
-#rescue LoadError
-#  puts "You need to install rspec in your base app"
-#  exit
-#end
-
-# This file is copied to ~/spec when you run 'ruby script/generate rspec'
-# from the project root directory.
 ENV["RAILS_ENV"] = "test"
 require File.expand_path(File.dirname(__FILE__) + "/railsenv/config/environment")
 require 'spec'
 require 'spec/rails'
-
 
 Spec::Runner.configure do |config|
   config.use_transactional_fixtures = true
